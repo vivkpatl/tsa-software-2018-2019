4ca516d30d03b925c6ca157185149011744f648c
Vivek Patel <therealvivek9064@gmail.com>
2018-11-03 22:56:08 -0700

    Tested out tray menu
    -Registered the app with the Windows tray bar and built a sample options menu
    @jellyco9064 please test it on windows 10 soon
    -Not merging the branch right now, will think if it's a good idea overnight

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ const electron = require('electron')
   let window
 
 //Some classes
-const { app, BrowserWindow } = require('electron')
+const { app } = require('electron')
+const path = require('path')
+const Tray = electron.Tray
+const iconPath = path.join(__dirname, "assets/tray/favicon.ico")
+const Menu = electron.Menu
 
-function createWindow() {
+let tray = null
+/*function createWindow() {
   //Create the dang window my dudes
   window = new BrowserWindow( { width: 800, height: 500} )
 
@@ -23,10 +28,34 @@ function createWindow() {
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
         window = null} )
-}
+}*/
 
 //Fire when ready
-app.on('ready', createWindow)
+app.on('ready', function() {
+    tray = new Tray(iconPath)
+
+    let template = [
+      {
+        label: 'Test Menu Made By Vivek',
+        submenu: [
+          {
+            label: 'Option 1'
+          },
+          {
+            label: 'Option 2'
+          },
+          {
+            label: "Option 3"
+          }
+        ]
+      }
+    ]
+
+      const contextMenu = Menu.buildFromTemplate(template)
+      tray.setContextMenu(contextMenu)
+})
+
+
 
 // Quit when all windows are closed.
  app.on('window-all-closed', () => {
@@ -41,6 +70,5 @@ app.on('ready', createWindow)
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (window === null) {
-      createWindow()
     }
   })

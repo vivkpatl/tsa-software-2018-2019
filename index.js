46c922cef8c5d3322e3c6a78b693223232b9cdad
JellyCo9064 <theredmoo@gmail.com>
2018-12-03 14:49:46 -0800

    Added basic clipboard watcher
    The 'history' file should be able to watch the clipboard for changes, and add items to an array. From here, needs to find a way to take the data from that array and dsiplay in the window.

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ function createWindow() {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     window = null} )
+	
 }
 
 //Fire when ready

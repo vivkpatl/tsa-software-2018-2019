30b74832158a46ee9e01f496cc4edcdef5c60f5c
Vivek Patel <therealvivek9064@gmail.com>
2018-12-15 15:45:51 -0800

    Testing out putting image data into the window
    Web development sucks don't @ me

--- mainViewController.js
+++ mainViewController.js
@@ -1,11 +1,1 @@
-//never hurts to require this module to avoid a TON of ipcRenderer messages to ipcMain
-const { clipboard } = require('electron')
-
-//let's try this..... it should copy the text of the <p> above onto the clipboard
-document.getElementById('testItem').addEventListener('click', function(err) {
-  clipboard.writeText(document.getElementById('testItem').innerHTML)
-})
-
-document.getElementById('testItem').addEventListener('click', function(err) {
-  clipboard.writeText(document.getElementById('testItem').innerHTML)
-})
+document.getElementById('testImage').src = "./assets/tray/favicon.ico"

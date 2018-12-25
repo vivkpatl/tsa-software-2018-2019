5ffa88c494424467b54a4c0b396f80a354e152cc
Vivek Patel <therealvivek9064@gmail.com>
2018-12-25 01:14:54 -0800

    12/25/2018
    -Merry Christmas!
    -Moved some functions
    -Added the item styling to the logic @jellyco9064 put in place
    -More styling dropping soon

--- index.js
+++ index.js
@@ -4,35 +4,37 @@ const { clipboard } = require('electron')
 const clipboardWatcher = require('electron-clipboard-watcher')
 
 
+function uponImgChange(nativeImage) {
+  clipboardHistory[clipboardHistory.length] = nativeImage;
+  let newItem = document.createElement("DIV")
+    newItem.className = "linkItem"
+  let img = document.createElement("IMG")
+  img.src = nativeImage
+  newItem.appendChild(img);
+
+  let list = document.getElementById("historyList")
+  list.appendChild(newItem);
+  console.log(clipboardHistory)
+}
+
+function uponTxtChange(text){
+  clipboardHistory[clipboardHistory.length] = text;
+  let newItem = document.createElement("DIV")
+    newItem.className = "linkItem"
+  let newText = document.createTextNode(text)
+  newItem.appendChild(newText);
+
+  let list = document.getElementById("historyList")
+  list.appendChild(newItem);
+  console.log(clipboardHistory)
+}
+
 var clipboardHistory = [];
 
 clipboardWatcher({
-  watchDelay: 1000,
+  watchDelay: 100,
 
-  onImageChange: function(nativeImage) {
-    clipboardHistory[clipboardHistory.length] = nativeImage;
-    //Use the IPC Renderer to send the new array to the IPC Main in the other 'window'
-    //ipcR.send('clipboard-history', clipboardHistory)
-    let newItem = document.createElement("DIV")
-    let img = document.createElement("IMG")
-    img.src = nativeImage
-    newItem.appendChild(img);
+  onImageChange: uponImgChange,
 
-    let list = document.getElementById("historyList")
-    list.appendChild(newItem);
-    console.log(clipboardHistory)
-  },
-
-  onTextChange: function(text){
-    clipboardHistory[clipboardHistory.length] = text;
-    //Use the IPC Renderer to send the new array to the IPC Main in the other 'window'
-    //ipcR.send('clipboard-history', clipboardHistory)
-    let newItem = document.createElement("DIV")
-    let newText = document.createTextNode(text)
-    newItem.appendChild(newText);
-
-    let list = document.getElementById("historyList")
-    list.appendChild(newItem);
-    console.log(clipboardHistory)
-  }
+  onTextChange: uponTxtChange
 })
